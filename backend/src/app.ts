8c724f4eabe7b0978885ebec6948d1de9e21ac53
alejandrocarabali01-gif <alejandro.carabali01@unicatolica.edu.co>
2025-11-21 23:59:33 -0500

    Subida completa del proyecto Parcial 3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
+import authRoutes from "./routes/auth.routes";
+import transactionsRoutes from "./routes/transaction.routes";
 const swaggerDocument = require("./swagger/swagger.json");
 
 const app = express();
@@ -8,12 +10,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// 👉 Redirigir la ruta raíz hacia Swagger
+
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionsRoutes);
+
+
 app.get("/", (req, res) => {
   res.redirect("/api-docs");
 });
 
-// Documentación Swagger
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
+
--- backend/src/routes/transaction.routes.ts
+++ backend/src/routes/transaction.routes.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
-import { addTransaction, getTransactions } from "../controllers/transaction.controller";
 
 const router = Router();
 
-router.get("/", getTransactions);
-router.post("/", addTransaction);
+// Transacciones
+router.get("/", (req, res) => res.json({ msg: "Lista de transacciones" }));
+router.post("/", (req, res) => res.json({ msg: "Transacción creada" }));
+router.get("/:id", (req, res) => res.json({ msg: "Detalle de transacción" }));
+router.put("/:id", (req, res) => res.json({ msg: "Transacción actualizada" }));
+router.delete("/:id", (req, res) => res.json({ msg: "Transacción eliminada" }));
+
+// Reportes
+router.get("/report/daily", (req, res) => res.json({ msg: "Reporte diario" }));
+router.get("/report/monthly", (req, res) => res.json({ msg: "Reporte mensual" }));
+router.get("/report/category", (req, res) => res.json({ msg: "Reporte por categoría" }));
+
+// Estadísticas
+router.get("/stats/totals", (req, res) => res.json({ msg: "Totales" }));
+router.get("/stats/summary", (req, res) => res.json({ msg: "Resumen" }));
 
 export default router;
